e59838e7693ef3b80534b53a265843fa23efa2b5
MatheusSalera <mspittar@gmail.com>
2025-03-21 21:51:43 -0300

    javaaaa

---
+++ exemplo3/script.js
@@ -0,0 +1,19 @@
+
+
+function Vernome() {
+    let numero = parseInt(document.getElementById("nome").value)
+    document.getElementById("nome").innerHTML = nome;
+}
+
+function Verlogin(){
+let login = parseInt(document.getElementById("login").value)
+document.getElementById("login").innerHTML = login;
+}
+
+
+function Versenha(){
+    let senha = parseInt(document.getElementById("senha").value)
+    document.getElementById("senha").innerHTML = senha;
+    }
+    
+    
