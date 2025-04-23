8d85eba0692e0ce3b224bf6ee784312fe41a80fe
MatheusSalera <mspittar@gmail.com>
2025-04-23 20:52:54 -0300

    lab7

---
+++ LAB_7/server.js
@@ -0,0 +1,11 @@
+const express = require('express'); const path = require('path');
+const app = express();
+const PORT = 80;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/Home.html', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'Home.html')); });
+
+app.get('/Project.html', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'Project.html')); });
+
+app.listen(PORT, () => { console.log(`Servidor rodando na porta ${PORT}`); });
