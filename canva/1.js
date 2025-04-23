3600b4c02c524927b911e8cf5e6c2ef9011e5c29
MatheusSalera <mspittar@gmail.com>
2025-04-23 19:19:00 -0300

    canva

--- canva/1.js
+++ canva/1.js
@@ -180,121 +180,108 @@ ctx.arc(150, 299, 65, 1.5 * Math.PI, 0 * Math.PI);
 ctx.stroke();
 ctx.closePath();
 
-{
 
-    let canvas2 = document.getElementById('canvas2');
-    let ctx = canvas2.getContext('2d');
+let canvas2 = document.getElementById('canvas2');
+let ctx = canvas2.getContext('2d');
 
-    // circulo amarelo
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.fillStyle = 'yellow';
-    ctx.arc(300, 105, 50, 1.10 * Math.PI, 3.2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = 'yellow';
+ctx.arc(300, 105, 50, 1.10 * Math.PI, 3.2 * Math.PI);
+ctx.fill();
+ctx.closePath();
 
-    // retangulo cinza
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = 'darkgrey';
-    ctx.fillRect(0, 300, 400, 100);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'darkgrey';
+ctx.fillRect(0, 300, 400, 100);
+ctx.closePath();
 
-    // tronco direita
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = 'SaddleBrown';
-    ctx.fillRect(340, 295, 20, 50);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'SaddleBrown';
+ctx.fillRect(340, 295, 20, 50);
+ctx.closePath();
 
-    // circulo arvore direita
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.fillStyle = 'green';
-    ctx.arc(350, 278, 30, 1.10 * Math.PI, 3.2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = 'green';
+ctx.arc(350, 278, 30, 1.10 * Math.PI, 3.2 * Math.PI);
+ctx.fill();
+ctx.closePath();
 
-    // tronco esquerda
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = 'SaddleBrown';
-    ctx.fillRect(55, 250, 20, 50);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'SaddleBrown';
+ctx.fillRect(55, 250, 20, 50);
+ctx.closePath();
 
-    // circulo arvore esquerda
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.fillStyle = 'green';
-    ctx.arc(65, 235, 30, 1.10 * Math.PI, 3.2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = 'green';
+ctx.arc(65, 235, 30, 1.10 * Math.PI, 3.2 * Math.PI);
+ctx.fill();
+ctx.closePath();
 
-    // trem azul de baixo
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = '#1C86EE';
-    ctx.fillRect(0, 350, 180, 50);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#1C86EE';
+ctx.fillRect(0, 350, 180, 50);
+ctx.closePath();
 
-    // circulo do trem azul de baixo
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.fillStyle = '#1C86EE';
-    ctx.arc(175, 390, 40, 1.10 * Math.PI, 3.2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = '#1C86EE';
+ctx.arc(175, 390, 40, 1.10 * Math.PI, 3.2 * Math.PI);
+ctx.fill();
+ctx.closePath();
 
-    // trem azul de cima
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = '#1C86EE';
-    ctx.fillRect(0, 290, 54, 110);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#1C86EE';
+ctx.fillRect(0, 290, 54, 110);
+ctx.closePath();
 
-    // circulo do trem azul de cima
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.fillStyle = '#1C86EE';
-    ctx.arc(14, 298, 40, 1.10 * Math.PI, 3.2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = '#1C86EE';
+ctx.arc(14, 298, 40, 1.10 * Math.PI, 3.2 * Math.PI);
+ctx.fill();
+ctx.closePath();
 
-    // quadrado marrom casa
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = 'SaddleBrown';
-    ctx.fillRect(147, 200, 100, 100);
-    ctx.closePath();
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'SaddleBrown';
+ctx.fillRect(147, 200, 100, 100);
+ctx.closePath();
 
-    //teto casa
-    ctx.beginPath();
-    ctx.moveTo(146, 200);
-    ctx.lineTo(248, 200);
-    ctx.lineTo(200, 145);
-    ctx.closePath();
-    ctx.fillStyle = "#EE6A50";
-    ctx.fill();
 
-    // janela esquerda
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = '#63B8FF';
-    ctx.fillRect(155, 220, 32, 32);
-    ctx.closePath();
+ctx.beginPath();
+ctx.moveTo(146, 200);
+ctx.lineTo(248, 200);
+ctx.lineTo(200, 145);
+ctx.closePath();
+ctx.fillStyle = "#EE6A50";
+ctx.fill();
 
-    // janela direita
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = '#63B8FF';
-    ctx.fillRect(206, 220, 32, 32);
-    ctx.closePath();
 
-    // porta
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = '#5C3C2B';
-    ctx.fillRect(186.5, 252, 20, 48);
-    ctx.closePath();
-}
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#63B8FF';
+ctx.fillRect(155, 220, 32, 32);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#63B8FF';
+ctx.fillRect(206, 220, 32, 32);
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = '#5C3C2B';
+ctx.fillRect(186.5, 252, 20, 48);
+ctx.closePath();
+
 
